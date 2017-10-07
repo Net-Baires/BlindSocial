5af334dafe28fe942b5edea837295b9652be3a1f
carlos.huerta <carlos.huerta@magretailgroup.com>
2017-10-07 11:14:42 -0300

    Added Api Key

---
+++ BlindSocial.Api/BlindSocial.Api/Controllers/BlindSocialController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BlindSocial.Api.Controllers
+{
+    public class BlindSocialController : ApiController
+    {
+        const string subscriptionKey = "41dc37f9889a48cfa74b64d424596029";
+        
+        const string uriBase = "https://brazilsouth.api.cognitive.microsoft.com/vision/v1.0";
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
