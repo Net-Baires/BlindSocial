e8006f2731c59cbd4e81785e715d3ad17c11f422
Sebastian Perez <sebastian@Rubens-MacBook-Pro-2.local>
2017-10-07 11:47:38 -0300

    Text to speech + permisos para la camara en ios

---
+++ BlindSocial/BlindSocial/BlindSocial/Views/IndexPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BlindSocial.Views
+{
+    public partial class IndexPage : ContentPage
+    {
+        public IndexPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTapGestureRecognizerTapped(object sender, EventArgs args) {
+            App.Current.MainPage = new SpeakPage();
+        }
+    }
+}
---
+++ BlindSocial/BlindSocial/BlindSocial/Views/SpeakPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Plugin.TextToSpeech;
+using Plugin.TextToSpeech.Abstractions;
+using Xamarin.Forms;
+
+namespace BlindSocial.Views
+{
+    public partial class SpeakPage : ContentPage
+    {
+        public SpeakPage()
+        {
+            InitializeComponent();
+
+			var speak = new Button
+			{
+				Text = "Hablar",
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+			};
+			speak.Clicked += async (sender, e) =>
+            {
+                 var languages = await CrossTextToSpeech.Current.GetInstalledLanguages();
+
+                CrossLocale? selectedLanguage = languages.Where(x => x.Language == "es" && x.Country=="ES").FirstOrDefault();
+
+                string text = "Hola mundo ! Somos el equipo Net Baires.";
+
+                await CrossTextToSpeech.Current.Speak(text, selectedLanguage);
+            };
+			Content = speak;
+        }
+    }
+}
